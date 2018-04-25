36dbfc7c21d8a5f04db73738d9e03ac85a936f78
Cory West-Forbes and Nick Kardish <cory@>
2018-04-25 16:06:38 -0700

    finish ui and backend promises

--- src/api.js
+++ src/api.js
@@ -4,6 +4,28 @@ class ApiCall {
     let request = new XMLHttpRequest();
       let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${userInput}`;
       request.onload = function() {
+        if (this.status === 200 ) {
+          console.log(request.response["meals"]);
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function(results) {
+      displayData(results)
+    }, function(error) {
+      return `something went wrong: ${error.message}`;
+    });
+  }
+  getData2(userInput, displayData){
+    let promise = new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+      let url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${userInput}`;
+      request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
         } else {
@@ -16,8 +38,30 @@ class ApiCall {
 
     promise.then(function(results) {
       displayData(results)
-    }, function() {
-      console.log('something went wrong');
+    }, function(error) {
+      console.log(`something went wrong: ${error.message}`);
+    });
+  }
+
+  getData3(id, displayData){
+    let promise = new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+      let url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function(results) {
+      displayData(results)
+    }, function(error) {
+      console.log(`something went wrong: ${error.message}`);
     });
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,28 +4,75 @@ import './styles.css';
 import $ from 'jquery';
 import { ApiCall } from './api.js'
 
+function clearData() {
+  $('#meal-name').empty();
+  $('#title').empty();
+  $('#categories').empty();
+  $('#ingredients').empty();
+  $('#measurements').empty();
+  $('.images').empty();
+  $('#meal-image').empty();
+  $('#instructions').empty();
+}
+
+function display(response) {
+  let body = JSON.parse(response);
+  if (body.meals ===  null) {
+    $('#title').text('Recipe not found');
+  } else{
+    $('#meal-image').append('<img src="' + body.meals[0].strMealThumb + '">');
+    $('#meal-name').text(body.meals[0].strMeal);
+    $('#instructions').text(body.meals[0].strInstructions);
+    for (let key in body.meals[0]) {
+      if(key.includes('strIngredient') &&  body.meals[0][key] != null && body.meals[0][key] != '') {
+        $('#title').text('ingredients');
+        $('#ingredients').append('<li>' + body.meals[0][key] + '</li>');
+      }
+      if(key.includes('strMeasure') &&  body.meals[0][key] != null && body.meals[0][key] != '') {
+        $('#title').text('ingredients');
+        $('#measurements').append('<li>' + body.meals[0][key] + '</li>');
+      }
+    }
+  }
+}
+
 $(function(){
   let apiCall = new ApiCall();
 
   let displayData = function(results) {
-    $('#ingredients').empty();
-    $('#measurements').empty();
+    clearData();
+    display(results);
+  }
+
+  let displayData2 = function(results){
+    clearData();
     let body=JSON.parse(results);
-    $("#meal-name").text(body.meals[0].strMeal);
-    for (let key in body.meals[0]) {
-      if(key.includes('strIngredient') &&  body.meals[0][key] != null && body.meals[0][key] != '') {
-        $('#ingredients').append("<li>" + body.meals[0][key] + "</li>");
-      }
-      if(key.includes('strMeasure') &&  body.meals[0][key] != null && body.meals[0][key] != '') {
-        $('#measurements').append("<li>" + body.meals[0][key] + "</li>");
-      }
+    for (let key in body.meals) {
+      $('#title').text('Dishes');
+      $('.images').append('<figure class="figselect" id="' + body.meals[key].idMeal + '"><img src="' + body.meals[key].strMealThumb + '"  ><figCaption>' + body.meals[key].strMeal + '</figCaption></figure>');
     }
+  }
+
+  let displayData3 = function(results) {
+    clearData();
+    display(results);
   }
 
   $('#find-meal').submit(function(event){
     event.preventDefault();
-    let userInput = $("#user-input").val();
+    let userInput = $('#user-input').val();
     apiCall.getData(userInput, displayData);
+  });
+  $('#category').submit(function(event){
+    event.preventDefault();
+    let category = $('#user-input2').val();
+    apiCall.getData2(category, displayData2);
+  });
+
+  $(".images").on('click', '.figselect', function () {
+    let mealId = ($(this).attr('id'));
+    console.log(mealId);
+    apiCall.getData3(mealId, displayData3);
   });
 
 });
